b82bbd7f3cf19819d693b2a4ee7ae73d04fc0965
RaraFolf <80000kevin@gmail.com>
2015-10-27 07:06:18 -0400

    Fixed player.mdl

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -12,30 +12,93 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-STUB_WEAPON_CLASS( cycler_weapon, WeaponCycler, C_BaseCombatWeapon );
+//Base Engine
+STUB_WEAPON_CLASS(cycler_weapon, WeaponCycler, C_BaseCombatWeapon);
+STUB_WEAPON_CLASS(weapon_ifm_steadycam, WeaponSteadycam, C_BaseCombatWeapon);
 
-STUB_WEAPON_CLASS( weapon_binoculars, WeaponBinoculars, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_bugbait, WeaponBugBait, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_flaregun, Flaregun, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_annabelle, WeaponAnnabelle, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_gauss, WeaponGaussGun, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_cubemap, WeaponCubemap, C_BaseCombatWeapon );
-STUB_WEAPON_CLASS( weapon_alyxgun, WeaponAlyxGun, C_HLSelectFireMachineGun );
-STUB_WEAPON_CLASS( weapon_citizenpackage, WeaponCitizenPackage, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_citizensuitcase, WeaponCitizenSuitcase, C_WeaponCitizenPackage );
-STUB_WEAPON_CLASS( weapon_ar2, WeaponAR2, C_HLMachineGun );
-STUB_WEAPON_CLASS( weapon_frag, WeaponFrag, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_rpg, WeaponRPG, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_pistol, WeaponPistol, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_shotgun, WeaponShotgun, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_smg1, WeaponSMG1, C_HLSelectFireMachineGun );
-STUB_WEAPON_CLASS( weapon_357, Weapon357, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_crossbow, WeaponCrossbow, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_slam, Weapon_SLAM, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_crowbar, WeaponCrowbar, C_BaseHLBludgeonWeapon );
-STUB_WEAPON_CLASS( weapon_hopwire, WeaponHopwire, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_proto1, WeaponProto1, C_BaseHLCombatWeapon );
-STUB_WEAPON_CLASS( weapon_oldmanharpoon, WeaponOldManHarpoon, C_WeaponCitizenPackage );
+//Half-Life 2
+STUB_WEAPON_CLASS(weapon_binoculars, WeaponBinoculars, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_bugbait, WeaponBugBait, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_flaregun, Flaregun, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_annabelle, WeaponAnnabelle, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_gauss, WeaponGaussGun, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_cubemap, WeaponCubemap, C_BaseCombatWeapon);
+STUB_WEAPON_CLASS(weapon_alyxgun, WeaponAlyxGun, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_citizenpackage, WeaponCitizenPackage, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_citizensuitcase, WeaponCitizenSuitcase, C_WeaponCitizenPackage);
+STUB_WEAPON_CLASS(weapon_ar2, WeaponAR2, C_HLMachineGun);
+STUB_WEAPON_CLASS(weapon_frag, WeaponFrag, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_rpg, WeaponRPG, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_pistol, WeaponPistol, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_shotgun, WeaponShotgun, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_smg1, WeaponSMG1, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_357, Weapon357, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_crossbow, WeaponCrossbow, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_slam, Weapon_SLAM, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_crowbar, WeaponCrowbar, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_hopwire, WeaponHopwire, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_proto1, WeaponProto1, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_oldmanharpoon, WeaponOldManHarpoon, C_WeaponCitizenPackage);
+
+//Counter-Strike: Source
+STUB_WEAPON_CLASS(weapon_ak47, WeaponAK47, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_aug, WeaponAug, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_awp, WeaponAWP, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_c4, WeaponC4, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_deagle, WeaponDeagle, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_elite, WeaponElite, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_famas, WeaponFamas, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_fiveseven, WeaponFiveseven, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_flashbang, WeaponFlashbang, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_g3sg1, WeaponG3SG1, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_galil, WeaponGalil, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_glock, WeaponGlock, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_hegrenade, WeaponHEGrenade, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_knife, WeaponKnife, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_m3, WeaponM3Super90, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_m249, WeaponM249, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_mac10, WeaponMac10, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_mp5navy, WeaponMP5Navy, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_p90, WeaponP90, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_p228, WeaponP228, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_scout, WeaponScout, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_sg550, WeaponSG550, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_sg552, WeaponSG552, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_smokegrenade, WeaponSmokeGrenade, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_tmp, WeaponTMP, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_ump45, WeaponUMP45, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_usp, WeaponUSP, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_xm1014, WeaponXM1014, C_BaseHLCombatWeapon);
+
+//Day of Defeat: Source
+STUB_WEAPON_CLASS(weapon_30cal, Weapon30Cal, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_amerknife, WeaponAmerKnife, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_bar, WeaponBar, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_basebomb, WeaponBaseBomb, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_bazooka, WeaponBazooka, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_c96, WeaponC96, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_colt, WeaponColt, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_frag_ger, WeaponFragGer, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_frag_ger_live, WeaponFragGerLive, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_frag_us, WeaponFragUS, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_frag_us_live, WeaponFragUSLive, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_garand, WeaponGarand, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_k98, WeaponK98, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_k98_scoped, WeaponK98Scoped, C_BaseHLCombatWeapon)
+STUB_WEAPON_CLASS(weapon_m1carbine, WeaponM1Carbine, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_mg42, WeaponMG42, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_mp40, WeaponMP40, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_mp44, WeaponMP44, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_pschreck, WeaponPSChreck, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_riflegren_ger, WeaponRiflegrenGer, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_riflegren_ger_live, WeaponRiflegrenGerLive, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_riflegren_us, WeaponRiflegrenUS, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_riflegren_us_live, WeaponRiflegrenUSLive, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_smoke_ger, WeaponSmokeGer, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_smoke_us, WeaponSmokeUS, C_HLSelectFireMachineGun);
+STUB_WEAPON_CLASS(weapon_spade, WeaponSpade, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_spring, WeaponSpring, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_thompson, WeaponThompson, C_HLSelectFireMachineGun);
 
 
 
